145f293bf055584eb81fe33aa55d5442dd906240
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2016-01-06 12:40:02 -0200

    add second lesson

---
+++ Aula02/app/src/main/java/br/com/aula02/FormActivity.java
@@ -0,0 +1,15 @@
+package br.com.aula02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
